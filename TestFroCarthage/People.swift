21e20897c27761a4299a48382ecdadbd305385ce
TerryLee <terry_lee@tutk.com>
2020-07-29 13:31:52 +0800

    add age

--- TestFroCarthage/People.swift
+++ TestFroCarthage/People.swift
@@ -11,12 +11,18 @@ import Foundation
 public class People {
     
     public var name: String
+    var age: Int
     
-    public init(name: String) {
+    public init(name: String, age: Int) {
         self.name = name
+        self.age = age
     }
     
     public func printName() {
         print(name)
+    }
+    
+    func printAge() {
+        print(age)
     }
 }
