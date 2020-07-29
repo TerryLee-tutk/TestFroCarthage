c7e8e374a04b2cf50f1a6d4249fc60f1dd879ec9
TerryLee <terry_lee@tutk.com>
2020-07-29 12:10:49 +0800

    init

---
+++ Carthage/Build/iOS/TestFroCarthage.framework/Headers/TestFroCarthage.h
@@ -0,0 +1,19 @@
+//
+//  TestFroCarthage.h
+//  TestFroCarthage
+//
+//  Created by Terry Lee on 2020/7/29.
+//  Copyright © 2020 Terry Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestFroCarthage.
+FOUNDATION_EXPORT double TestFroCarthageVersionNumber;
+
+//! Project version string for TestFroCarthage.
+FOUNDATION_EXPORT const unsigned char TestFroCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFroCarthage/PublicHeader.h>
+
+
---
+++ TestFroCarthage/People.swift
@@ -0,0 +1,22 @@
+//
+//  People.swift
+//  TestFroCarthage
+//
+//  Created by Terry Lee on 2020/7/29.
+//  Copyright © 2020 Terry Lee. All rights reserved.
+//
+
+import Foundation
+
+public class People {
+    
+    public var name: String
+    
+    public init(name: String) {
+        self.name = name
+    }
+    
+    public func printName() {
+        print(name)
+    }
+}
